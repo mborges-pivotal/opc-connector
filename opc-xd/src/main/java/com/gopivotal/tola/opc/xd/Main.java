488a8640781461c2ce06397a3507d66795d58018
mborges-pivotal <mborges@gopivotal.com>
2015-08-31 16:05:24 -0500

    fixing travis ci build

--- opc-xd/src/main/java/com/gopivotal/tola/opc/xd/Main.java
+++ opc-xd/src/main/java/com/gopivotal/tola/opc/xd/Main.java
@@ -5,39 +5,26 @@
  * one or more patents listed at http://www.gopivotal.com/patents.
  *=========================================================================
  */
-package com.gopivotal.tola.opc.boot;
+package com.gopivotal.tola.opc.xd;
 
 import java.io.File;
+
 import java.util.Properties;
 import java.util.jar.Attributes;
 import java.util.jar.JarFile;
 import java.util.jar.Manifest;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-
 /**
- * Main - the spring boot application
+ * Main - to satisfied spring-boot:repackage goal and to get manifest information from single jar
  * 
  * @author mborges
  *
  */
-@ComponentScan({ "com.gopivotal.tola.opc", "com.gopivotal.tola.opc.boot" })
-@SpringBootApplication
 public class Main {
 
-	public static void main(String[] args) throws Exception {
-		SpringApplication spa = new SpringApplication(Main.class);
-		
-		readManifest();
+	public static final String FS = File.separator;
 
-		spa.setWebEnvironment(false);
-		spa.run(args);
-
-	}
-
-	private static void readManifest() {
+	public static void main(String args[]) {
 		try {
 			Properties props = System.getProperties();
 			String classpath = props.getProperty("java.class.path");
@@ -47,10 +34,10 @@ public class Main {
 			// was run from the jar's location on the HD
 			// if there are File.separators then it was run from another place
 			// on the HD
-			if (classpath.indexOf(File.separator) != -1)
+			if (classpath.indexOf(FS) != -1)
 				pathtojar = classpath;
 			else
-				pathtojar = userdir + File.separator + classpath;
+				pathtojar = userdir + FS + classpath;
 			// Now we have the full path to the jar, print it to see
 			System.out.println("Path To Jar = " + pathtojar);
 			JarFile jar = new JarFile(pathtojar);
@@ -64,5 +51,4 @@ public class Main {
 			ex.printStackTrace();
 		}
 	}
-
 }
